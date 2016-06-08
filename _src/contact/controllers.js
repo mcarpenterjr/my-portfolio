f5b12ae54bd9f748c99ea7b3f164222e211eecf2
Mark N Carpenter Jr <marksthings24@gmail.com>
2016-06-07 22:56:48 -0400

    refactor: Renamed PHP Contact Form

--- _src/contact/controllers.js
+++ _src/contact/controllers.js
@@ -10,7 +10,7 @@ app.controller('ContactController', function ($scope, $http) {
         if (contactform.$valid) {
             $http({
                 method  : 'POST',
-                url     : 'contact/contact-form.php',
+                url     : '/contact/cform.php',
                 data    : $.param($scope.formData),  //param method from jQuery
                 headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  //set the headers so angular passing info as form data (not request payload)
             }).success(function(data){

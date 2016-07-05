82d2b00f3b8f7454117087a6ca1369749ef298c8
Mark Carpenter Jr <marksthings24@gmail.com>
2016-07-04 22:40:39 -0400

    feature: Utility Updates
    FTP Deploy
    Dev Preview Auto Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
+var gutil = require( 'gulp-util' );
+var ftp = require( 'vinyl-ftp' );
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 var del = require('del');
-
 
 var paths = {
   //IMPORTANT: Use to block tasks from parsing this massive directory!
@@ -25,12 +26,39 @@ gulp.task('fontgen', function() {
 
 //  Developement Preview
 gulp.task('serve', function() {
-  browserSync.init({
+  browserSync.init(['./_src/**', './_src/css/*.css', './_src/js/*.js', './_src/fonts/**', './_src/img/**'], {
     server: './_src'
   });
-  // Watches for changes
-  gulp.watch(['content', 'styles', 'scripts'], reload);
 });
+
+// Deployment to Remote Server
+gulp.task( 'deploy', function () {
+
+	var conn = ftp.create( {
+		host:     'mncarpenter.ninja',
+		user:     'something@mncarpenter.ninja',
+		password: 'NULL',
+		parallel: 10,
+		log:      gutil.log
+	} );
+
+	var globs = [
+		'_src/**',
+		'_src/css/**',
+		'_src/js/**',
+		'_src/fonts/**',
+		'_src/img/**',
+		'_src/index.html'
+	];
+
+	// using base = '.' will transfer everything to /public_html correctly
+	// turn off buffering in gulp.src for best performance
+
+	return gulp.src( globs, { base: './_src', buffer: false } )
+		.pipe( conn.newerOrDifferentSize('/') ) // only upload newer files
+		.pipe( conn.dest('/') );
+
+} );
 
 // The default task (called when you run `gulp` from cli)
 gulp.task('default', ['serve']);
